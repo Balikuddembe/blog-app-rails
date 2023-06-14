3f1739b655c95582a95299a83ec585a888e32672
Balikuddembe <balixemma@gmail.com>
2023-06-14 15:19:06 +0300

    Create actions for the posts controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostController < ActionController
+    def index
+        @user = User.find(params[:user_id])
+        @posts = Post.where(author_id: @user)
+    end
+
+    def show
+        @user = User.find(params[:user_id])
+        @post = Post.find_by!(id: params[:id], author_id: params[:user_id])
+    end
+end
